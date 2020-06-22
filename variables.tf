73fdc010481e05f1b82e66db29de7b32d2142ce2
Ann Panasyuk <30906983+AnnPanasyuk@users.noreply.github.com>
2020-06-22 17:06:58 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+variable "region" {
+  description = "Region string"
+  default = "Region default value"
+}
+variable "prefix" {
+  description = "Prefix string"
+  default = ""  
+}
+variable "name" {}
 
