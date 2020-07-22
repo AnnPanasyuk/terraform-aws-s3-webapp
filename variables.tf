5e383998fdf0122ba3c2ac74a9aa754113534022
Ann Panasyuk <30906983+AnnPanasyuk@users.noreply.github.com>
2020-07-22 14:08:42 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,13 @@
 variable "region" {
   description = "Region string"
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 variable "prefix" {
-  description = "Prefix string"
-  default = ["a", "b", "c"]
+  type        = "list"
+  description = "Prefix array"
+  default.    = ["a", "b", "c"]
 }
 variable "name" {
-  default = "name"  
+  default = "name1"  
 }
 
